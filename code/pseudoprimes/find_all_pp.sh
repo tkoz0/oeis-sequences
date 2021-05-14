c80bccb3705ce5f4b84212a2961cc746b7bfabcd
TKoz Zero <tkozlowski1999@gmail.com>
2021-05-13 22:05:15 -0400

    finds all pseudoprimes up to 2^42 given a base

---
+++ code/pseudoprimes/find_all_pp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo BASE=$1
+mkdir out_$1
+python3 job_generator.py \
+    | parallel --slf .. \
+        --basefile fpp_tmp/fpp_$1 \
+        --basefile sorted_diff \
+        --basefile find_pp.sh \
+        --cleanup \
+        --header : \
+        --colsep , \
+        --tag \
+        --files \
+        --tmpdir out_$1 \
+        --joblog jobs_$1.log \
+        --bar \
+        ./find_pp.sh $1 {lo} {hi} \
+    > parallel_$1.stdout

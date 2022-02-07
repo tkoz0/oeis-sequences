0c65cac486fad6796ff61d1b17513f264e82e970
tkoz0 <null>
2022-02-06 20:40:33 -0500

    renamed and remove 2^64 limitation

--- code/truncatable_primes/num_len_filter.py
+++ code/truncatable_primes/num_len_filter.py
@@ -1,12 +1,15 @@
-# filter numbers by their length in a base
+# num_len_filter.py <base> <length1> [length2...]
+# read integers (as base 10) from stdin (1 per line) and using given base
+# output (in base 10) the numbers with length1 (or length2...) digits
 import sys
 base = int(sys.argv[1])
 assert base >= 2
 def has_n_digits(num,n):
     return base**(n-1) <= num < base**n
 nvals = [int(a) for a in sys.argv[2:]]
+for nval in nvals:
+    assert nval > 0
 for line in sys.stdin:
     num = int(line)
-    assert 0 <= num < 2**64 # root values must fit in uint64_t
     if any(has_n_digits(num,nval) for nval in nvals):
         print(line,end='')

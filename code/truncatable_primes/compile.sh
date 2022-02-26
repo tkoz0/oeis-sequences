3e1e626f342096436f7f874aac0bba41993e0f4a
tkoz0 <null>
2022-02-26 02:19:37 -0500

    messages and tree_convert compile command

--- code/truncatable_primes/compile.sh
+++ code/truncatable_primes/compile.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo --- Compiling tp_tree \(-DWRITE_TREE\)
 gcc -Wall -Werror -g -O3 truncprimes.c tp_util.c -lgmp -o tp_tree -DWRITE_TREE
+echo --- Compiling tp_stats \(-DWRITE_STATS\)
 gcc -Wall -Werror -g -O3 truncprimes.c tp_util.c -lgmp -o tp_stats -DWRITE_STATS
+echo --- Compiling tree_convert
+gcc -Wall -Werror -g -O3 tree_convert.c -lgmp -o tree_convert
